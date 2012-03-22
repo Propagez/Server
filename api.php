db7744ec22e621501cca608914028fe2aee1f5b1
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2012-03-21 21:37:16 -0400

    Détails sur les données liés à un événement

--- api.php
+++ api.php
@@ -11,7 +11,29 @@
  * Chaque méthode reçoit un ou des paramètres d'entrés provenant de la requête
  * et doit retourner l'information demandée.
  *
- * Si une erreur se produit, la méthode doit retourner une Exception.
+ * Si une erreur se produit, la méthode doit retourner une Exception avec un message
+ * expliquant l'erreur.
+ *
+ *
+ * Liste des champs d'un événement:
+ *
+ * id		: Identifiant unique de l'événement
+ * title	: Titre de l'événement
+ * description : Description de l'événement
+ * date		: Date de l'événement (AAAA-MM-JJ)
+ * time		: Heure de l'événement (HH:MM)
+ * aid		: Identifiant unique de l'artiste
+ * artist	: Nom de l'artiste
+ * genres[]	: Array de styles musicaux
+ * bio		: Bio de l'artiste
+ * website	: Site web de l'artiste
+ * vid		: Identifiant unique de la salle
+ * venue	: Nom de la salle
+ * address	: Adresse
+ * city		: Ville
+ * postalcode : Code Postal
+ * lat		: Latitude
+ * lng		: Longitude
  *
  *
  */
